9baac0cf6a5e0647be618869e19c3d8bcff90686
hboiled <sflee36@yahoo.com.au>
2020-02-28 12:58:03 +0800

    added portfolio activity 4

---
+++ WebPortfolioA4/QuoteOfToday.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="styles/style.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>    
+</head>
+<body>
+    <h1>Quote of Today</h1>
+    <button onClick="window.location.reload();" class="btns" type="button">Click here to display a random quote</button> 
+    <?php
+    $connection = mysqli_connect("localhost", "root", "", "quotes");
+    
+    echo "</br>";
+    
+    if (!$connection) {
+        printf("Connection failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    $randNum = rand(1, 10);
+    $statement = "Select * from famousquotes Where ID=".$randNum.";";
+
+    //printf("host info: %s\n", mysqli_get_host_info($connection));
+    //printf("%d", $randNum);
+
+    if ($res = mysqli_query($connection, $statement)) {        
+        if ($record = mysqli_fetch_row($res)) {
+            printf("<p class='content'>%s</p>", $record[1], );
+            printf("<p class='source'>%s</p>", $record[2]);
+            
+        }
+        
+    }
+    
+    mysqli_close($connection);
+    ?>
+</body>
+</html>
---
+++ WebPortfolioA4/quotes.sql
@@ -0,0 +1,76 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 28, 2020 at 05:20 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `quotes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `famousquotes`
+--
+
+CREATE TABLE `famousquotes` (
+  `ID` int(11) NOT NULL,
+  `Content` varchar(64) DEFAULT NULL,
+  `Source` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `famousquotes`
+--
+
+INSERT INTO `famousquotes` (`ID`, `Content`, `Source`) VALUES
+(1, 'Be yourself; everyone else is already taken.', 'Oscar Wilde'),
+(2, 'So many books, so little time.', 'Frank Zappa'),
+(3, 'A room without books is like a body without a soul.', 'Marcus Tullius Cicero'),
+(4, 'You only live once, but if you do it right, once is enough.', 'Mae West'),
+(5, 'Be the change that you wish to see in the world.', 'Mahatma Gandhi'),
+(6, 'If you tell the truth, you don\'t have to remember anything.', 'Mark Twain'),
+(7, 'A friend is someone who knows all about you and still loves you.', 'Elbert Hubbard'),
+(8, 'Without music, life would be a mistake.', 'Friedrich Nietzsche'),
+(9, 'The difference between genius and stupidity is: genius has its l', 'Alexandre Dumas-fils'),
+(10, 'If you can make a woman laugh, you can make her do anything.', 'Marilyn Monroe');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `famousquotes`
+--
+ALTER TABLE `famousquotes`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `famousquotes`
+--
+ALTER TABLE `famousquotes`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
